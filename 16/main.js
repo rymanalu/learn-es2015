95fdf0b6af3a1fc6c9a68578d6efc331a6d71438
Roni Yusuf Manalu <rymanalu@gmail.com>
2017-04-18 00:41:34 +0700

    Eps 16

---
+++ 16/main.js
@@ -0,0 +1,13 @@
+function *range(start, end) {
+    while (start <= end) {
+        yield start;
+
+        start++;
+    }
+}
+
+let iterator = range(1, 5);
+
+for (let i of iterator) {
+    console.log(i);
+}
